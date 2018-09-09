7661a7821db9b324f3ff262e90ab122ce8261611
Marlon Geraldo Rodrigues Viana <marlongeraldorodriguesviana@Marlons-MacBook-Pro.local>
2018-09-09 15:32:25 +0100

    Added checkout form contact data handlers

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
     state = {
@@ -20,7 +21,7 @@ class ContactData extends Component {
         this.setState( { loading: true} );
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.totalPrice,
+            price: this.props.price,
             customer: {
                 name: 'Marlon',
                 address: {
@@ -33,26 +34,37 @@ class ContactData extends Component {
             deliveryMethod: 'fastest'
         }
 
+        // Post for example only. It will always fail as Firebase DB is not setup
         axios.post('/orders.json', order)
             .then( response => {
-                this.setState( {loading: false, purchasing: false })
+                this.setState( {loading: false });
+                this.props.history.push('/');
             })
             .catch( error => {
-                this.setState( {loading: false, purchasing: false })
+                this.setState( {loading: false });
+                this.props.history.push('/');
             });
     }
     
     render () {
+        let form = (
+            <form>
+                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+                <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+                <input className={classes.Input} type="text" name="street" placeholder="street" />
+                <input className={classes.Input}  type="text" name="postal" placeholder="Postal Code" />
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+
+        if (this.state.loading) {
+            form = <Spinner />
+        }
+
         return (
             <div className={classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
-                <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-                    <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
-                    <input className={classes.Input} type="text" name="street" placeholder="street" />
-                    <input className={classes.Input}  type="text" name="postal" placeholder="Postal Code" />
-                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-                </form>
+                {form}
             </div>
         )
     }
